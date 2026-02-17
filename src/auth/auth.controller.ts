ee172ad91d6e1075bb613f40088fb8eff45fca0e
Tu Nombre <tuemail@gmail.com>
2026-02-17 01:22:31 -0300

    feat(auth): roles guard funcionando (admin-only endpoint)

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,8 @@
-import { Body, Controller, Post } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseGuards } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { JwtAuthGuard } from "./jwt-auth.guard";
+import { Roles } from "./roles.decorator";
+import { RolesGuard } from "./roles.guard";
 
 @Controller("auth")
 export class AuthController {
@@ -8,5 +11,19 @@ export class AuthController {
     @Post("login")
     login(@Body() body: { username: string; password: string }) {
         return this.auth.login(body.username, body.password);
+    }
+
+    @Get("profile")
+    @UseGuards(JwtAuthGuard)
+    profile(@Req() req: any) {
+        return req.user;
+    }
+
+    // 👇 NUEVO ENDPOINT SOLO ADMIN
+    @Get("admin-only")
+    @UseGuards(JwtAuthGuard, RolesGuard)
+    @Roles("ADMIN")
+    adminOnly() {
+        return { ok: true, message: "Sos ADMIN" };
     }
 }
